eeb1c381a1e21fb3af378dac626d91b75b62cb7a
George Leontiev <folone@gmail.com>
2013-12-11 16:13:14 +0100

    Test for nested case class.

--- core/src/main/scala/test.scala
+++ core/src/main/scala/test.scala
@@ -5,14 +5,17 @@ import u._
 
 
 case class Ok(s: String, i: Int)
+case class Test(d: Double, s: String, ok: Ok)
 
 object Test {
   import liftableMacro._
 
   val s = Ok("s", 1)
+  val tst = Test(1.0, "test", s)
 
   def ok = {
     implicit val ev = liftableMacro.liftableCaseClass[Ok]
-    showRaw(q"""$s""")
+    println(showRaw(q"""$s"""))
+    println(showRaw(q"""$tst"""))
   }
 }
