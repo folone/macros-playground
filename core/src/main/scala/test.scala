f1c98cf232160756a6c55d4de224a266b704c1ad
George Leontiev <folone@gmail.com>
2013-12-11 21:21:16 +0100

    Test for a case where `fullName` contains dots.

--- core/src/main/scala/test.scala
+++ core/src/main/scala/test.scala
@@ -6,16 +6,19 @@ import u._
 
 case class Ok(s: String, i: Int)
 case class Test(d: Double, s: String, ok: Ok)
+case class `Ok.Test`(d: Double)
 
 object Test {
   import liftableMacro._
 
   val s = Ok("s", 1)
   val tst = Test(1.0, "test", s)
+  val dot = `Ok.Test`(1.0)
 
   def ok = {
     implicit val ev = liftableMacro.liftableCaseClass[Ok]
     println(showRaw(q"""$s"""))
     println(showRaw(q"""$tst"""))
+    println(showRaw(q"""$dot"""))
   }
 }
