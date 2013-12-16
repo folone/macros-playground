3b8df60a6149616461da63491f6a84ad43eaf00f
George Leontiev <folone@gmail.com>
2013-12-16 09:29:50 +0100

    Works on the simplified example, but still getting some errors. Investigating.

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -41,19 +41,18 @@ object liftableMacro {
     val spliced = params.map { case (name, typeSign) ⇒
       q"implicitly[Liftable[$typeSign]].apply(universe, value.$name)"
     }
-    val string = "somestring"
-    //println(showRaw(q"$string"))
-    c.Expr[Liftable[T]](q"""
-        import scala.reflect.api.Universe
-        new Liftable[$T] {
-          def apply(universe: Universe, value: $T): universe.Tree = {
-            import universe._
-            Apply(Select(New(Ident(TermName("pkg.ok.Ok"))),
-                         newTermName("apply")), List(..$spliced))
 
-          }
+    c.Expr[Liftable[T]] { q"""
+      import scala.reflect.api.Universe
+      new Liftable[$T] {
+        def apply(universe: Universe, value: $T): universe.Tree = {
+          import universe._
+          Apply(Select(New(Ident(TermName(${symbol.fullName}))),
+                       newTermName("apply")), List(..$spliced))
         }
-    """)
+      }
+    """
+    }
   }
 
 }
